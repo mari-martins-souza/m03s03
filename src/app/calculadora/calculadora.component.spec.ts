bc9f70117695204ba116b23dc18ee2adabf052e2
Mariana Martins <marih_ms@hotmail.com>
2024-09-10 22:24:43 -0300

    exercicio 6

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../services/calculadora.service';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
   let fixture: ComponentFixture<CalculadoraComponent>;
+  let calculadoraService: CalculadoraService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CalculadoraComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [CalculadoraComponent],
+      providers: [CalculadoraService]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    calculadoraService = TestBed.inject(CalculadoraService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('atualiza o resultado corretamente ao somar dois números', () => {
+    component.num1 = 5;
+    component.num2 = 3;
+    component.somar();
+    expect(component.result).toBe(8);
   });
 });
