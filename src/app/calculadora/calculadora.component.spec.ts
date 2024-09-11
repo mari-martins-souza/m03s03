6776d61a62a95fbf15a442cdc282cede36f0c5d8
Mariana Martins <marih_ms@hotmail.com>
2024-09-11 19:44:41 -0300

    exercicio 7

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CalculadoraComponent } from './calculadora.component';
 import { CalculadoraService } from '../services/calculadora.service';
+import { FormsModule } from '@angular/forms';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -9,15 +10,17 @@ describe('CalculadoraComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [CalculadoraComponent],
+      imports: [CalculadoraComponent, FormsModule],
       providers: [CalculadoraService]
-    }).compileComponents();
+    })
+    .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
     calculadoraService = TestBed.inject(CalculadoraService);
+    fixture.detectChanges();
   });
 
   it('atualiza o resultado corretamente ao somar dois números', () => {
@@ -25,5 +28,22 @@ describe('CalculadoraComponent', () => {
     component.num2 = 3;
     component.somar();
     expect(component.result).toBe(8);
+  });
+
+  it('atualiza num1 e num2 quando a entrada do input for alterada pelo usuário ', () => {
+    const compiled = fixture.nativeElement;
+
+    const inputNum1 = compiled.querySelector('input[name="num1"]');
+    inputNum1.value = '5';
+    inputNum1.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    const inputNum2 = compiled.querySelector('input[name="num2"]');
+    inputNum2.value = '10';
+    inputNum2.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(component.num1).toBe(5);
+    expect(component.num2).toBe(10);
   });
 });
